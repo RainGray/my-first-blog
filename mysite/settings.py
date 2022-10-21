9e4c5a4f957db98239d8c1f7ac1b7f52841adf2c
Rain Gray <blek42dgek@gmail.com>
2022-10-21 10:47:12 +0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0jnd0=62_86ga)gzc)6adggai!io4j&*_@xjoqp^j&1tpc74d6'
+with open('etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
